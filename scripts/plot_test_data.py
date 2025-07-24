244cfd94aefd1f40f3cccee5d7999e42d0922713
TayTay0710 <taylah0710@gmail.com>
2025-07-25 07:51:27 +0800

    Update to match new data format.

--- scripts/plot_test_data.py
+++ scripts/plot_test_data.py
@@ -10,21 +10,37 @@ def read_line(line):
     values = clean.split(',')
     
     timestamp = values[0]
-    distances = list(map(int, values[1:6]))
-    yaw = float(values[7])
+    x, y, z, = map(float, values[1:4])
+    yaw = float(values[4])
+    distances = list(map(int, values[5:11]))
     
     print("Timestamp:", timestamp)
-    print("Distances:", distances)  # Example: front sensor
-    print("Yaw values:", yaw)
+    print("Position (x, y, z): ", x, y, z)
+    print("Yaw:", yaw)
+    print("Distances:", distances)
     
-    return timestamp, distances, yaw   
+    return timestamp, (x, y, z), distances, yaw   
     
     
 # Read in the test data file
 def read_file():
+    # Create arrays for sets of data
+    timestamps = []
+    positions = []
+    yaws = []
+    distance_sets = []
+    
+    
     with open('../sensor_logs/test_data_10_lines.txt', 'r') as file:
         for line in file:
-            read_line(line)
+            timestamp, position, yaw, distances = read_line(line)
+            
+            timestamps.append(timestamp)
+            positions.append(position)
+            yaws.append(yaw)
+            distance_sets.append(distances)
+            
+    return timestamps, positions, yaws, distance_sets
             
 if __name__ == '__main__':
     read_file()
