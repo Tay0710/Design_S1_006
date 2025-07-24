3caf1d0093bb41ea0f57652ddb3d67f66cefe584
Dorothy Jong <dorothy.jong@hotmail.com>
2025-07-24 22:02:34 +0800

    Basic TCP comms functioning

--- ESP32_TCP_Client/src/config.h
+++ ESP32_TCP_Client/src/config.h
@@ -15,7 +15,7 @@
 #define SSID "ESP-TEST"
 #define PASSWORD "123456789"
 
-#define SERVER_HOST_NAME "esp_server"
+#define SERVER_HOST_NAME "192.168.4.2"  // IP address of TCP server (on PC)
 
 #define TCP_PORT 7050
 #define DNS_PORT 53
--- ESP32_TCP_Client/src/main.cpp
+++ ESP32_TCP_Client/src/main.cpp
@@ -1,8 +1,17 @@
 #include <Arduino.h>
 #include <AsyncTCP.h>
 #include <WiFi.h>
+#include <stdio.h>
+
 
 #include "config.h"
+
+#define TRIG_PIN 11
+#define ECHO_PIN 10
+
+
+AsyncClient* client = new AsyncClient;
+
 
 
 static void replyToServer(void* arg) {
@@ -29,30 +38,70 @@ void onConnect(void* arg, AsyncClient* client) {
 }
 
 void setup() {
+
+	pinMode(TRIG_PIN, OUTPUT);
+  	pinMode(ECHO_PIN, INPUT);
 	Serial.begin(115200);
 	delay(500); // Delay required so that print statements will show up
 
-  // Setup ESP32 as the access point 
-  // https://randomnerdtutorials.com/esp32-access-point-ap-web-server/
-  Serial.println("Setting Access Point...");
-  WiFi.softAP(SSID, PASSWORD);
+	// Setup ESP32 as the access point 
+	// https://randomnerdtutorials.com/esp32-access-point-ap-web-server/
+	Serial.println("Setting Access Point...");
+	WiFi.softAP(SSID, PASSWORD);
 
-  // Print ESP Local IP Address
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP Address: ");
-  Serial.println(IP);
+	// Print ESP Local IP Address
+	IPAddress IP = WiFi.softAPIP();
+	Serial.print("AP IP Address: ");
+	Serial.println(IP);
 
-  // Setup ESP32 as AsyncTCP Client 
-	AsyncClient* client = new AsyncClient;
-	client->onData(&handleData, client);
-	client->onConnect(&onConnect, client);
+	// Setup ESP32 as AsyncTCP Client 
+	client->onData(&handleData, client); // when data received
+	client->onConnect(&onConnect, client); // on successful connect
+
+	// TODO: either make this a blocking call or ensure PC is connected first
+	// TOOD: check if I dont connect 
 	client->connect(SERVER_HOST_NAME, TCP_PORT);
 
-  // TODO: wait for PC connection to AP if possible
+	Serial.println("Connecting to TCP server");
+
+	while (!client->connecting())
+	{
+		Serial.print(".");
+		delay(500);
+	}
+
+	// TODO: wait for PC connection to AP if possible
 
 }
 
 void loop() {
-  // TODO
+
+	float duration, distance;
+
+	// read ultrasonic sensor data
+	digitalWrite(TRIG_PIN, LOW);
+	delayMicroseconds(2);
+	digitalWrite(TRIG_PIN, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(TRIG_PIN, LOW);
+
+	duration = pulseIn(ECHO_PIN, HIGH);
+	distance = (duration*.0343)/2; // in cm
+	Serial.print("Distance: ");
+	Serial.println(distance);
+
+	// Send data
+	if (client->space() > 32 && client->canSend()) {
+		char buffer[32];
+		sprintf(buffer, "%.2f", distance); 
+		client->add(buffer, strlen(buffer));
+		client->send();
+	}
+
+	// TODO: What happens if the space is not large enough?
+
+
+	delay(100);
 
 }
+
