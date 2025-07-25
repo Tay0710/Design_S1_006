3dab27718454c3f6d191ee57b6071bba7a7cb770
TayTay0710 <taylah0710@gmail.com>
2025-07-25 08:03:53 +0800

    Update to switch mismatched variables.

---
+++ scripts/plot_2d.py
@@ -0,0 +1,6 @@
+from format_data import read_file
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    timestamps, positions, yaws, distances = read_file()
+    # Plotting code continues...
