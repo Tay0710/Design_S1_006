dad2b292cfa2027523e740269297855152803047
Dorothy Jong <88929687+dj8389@users.noreply.github.com>
2025-07-27 22:44:07 +0800

    write to csv complete

--- TCP_Connection/tcp_server.py
+++ TCP_Connection/tcp_server.py
@@ -5,19 +5,25 @@ import numpy as np
 import threading
 import re
 import time
-
+import csv
+from datetime import datetime
 
 # Note: need to make sure both PC and ESP32 are on the same network (better to set up ESP32 as AP - need to test range)
-# TODO: implement multithreading into this to process the data...
 
 HOST = "192.168.4.2"  # IP address of PC
 PORT = 7050  # Port to listen on (non-privileged ports are > 1023)
+current_datetime = datetime.now().replace(microsecond=0)
+
+filename = str(current_datetime) + ".csv"
+filename = filename.replace(":", "-")
 
 
 # Initialise lists to store plot
 index = []
 x = []
 y = []
+z = []
+yaw = []
 distance = []
 
 def TCP_server():
@@ -28,29 +34,34 @@ def TCP_server():
         with conn:
             print(f"Connected by {addr}")
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(1024)            
                 s = data.decode('utf-8')  # decode bytes to string using UTF-8 encoding
                 groups = re.findall(r'\((.*?)\)', s)
 
                 for g in groups:
                     parts = g.split(", ")           # split by comma+space
                     data = [float(x) for x in parts]
+                    row = [str(x) for x in parts]
                     index.append(data[0])
                     x.append(data[1])
                     y.append(data[2])
-                    distance.append(data[-1])
+                    z.append(data[3])
+                    yaw.append(data[4])
+                    distance.append(data[5])
+
+                    with open(filename, mode="a", newline="") as file:
+                        writer = csv.writer(file)                        
+                        writer.writerow(row)
 
                 if not data:
                     break
                 #conn.sendall(b'Acknowledged')
 
 def display_map():
-    print("TODO: write code")
-
     while(True):
-        print(x)
-        print(y)
-        print(distance)
+        # print(x)
+        # print(y)
+        # print(distance)
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -58,7 +69,13 @@ def display_map():
         plt.show()
         time.sleep(1)
 
+
 def main():
+    # Write new file
+    with open(filename, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        data = ['index', 'x', 'y', 'z', 'yaw', 'distance']
+        writer.writerow(data)
 
     # Start TCP server thread
     t_server = threading.Thread(target=TCP_server)
