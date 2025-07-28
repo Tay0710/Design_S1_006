b4f6d29f6a26eff9bde5d925758f8e9703e6dcd3
Sam27315 <141702190+Sam27315@users.noreply.github.com>
2025-07-28 19:15:48 +0800

    Adding libraries from dRehm and a file for collecting filtered data using these libraries

---
+++ IMU_path_tracking/dRehm/dRehm_track_path/dRehm_track_path.ino
@@ -0,0 +1,234 @@
+// MPU-6050 code developed from dRehmFlight (https://github.com/nickrehm/dRehmFlight/tree/master)
+
+#include "src/MPU6050/MPU6050.h"
+MPU6050 mpu6050;
+#include <Wire.h>
+
+//Uncomment only one IMU
+#define USE_MPU6050_I2C //Default
+
+#if defined USE_MPU6050_I2C
+  #define GYRO_FS_SEL_250    MPU6050_GYRO_FS_250
+  #define GYRO_FS_SEL_500    MPU6050_GYRO_FS_500
+  #define GYRO_FS_SEL_1000   MPU6050_GYRO_FS_1000
+  #define GYRO_FS_SEL_2000   MPU6050_GYRO_FS_2000
+  #define ACCEL_FS_SEL_2     MPU6050_ACCEL_FS_2
+  #define ACCEL_FS_SEL_4     MPU6050_ACCEL_FS_4
+  #define ACCEL_FS_SEL_8     MPU6050_ACCEL_FS_8
+  #define ACCEL_FS_SEL_16    MPU6050_ACCEL_FS_16
+#endif
+
+//Uncomment only one full scale gyro range (deg/sec)
+#define GYRO_250DPS //Default
+//#define GYRO_500DPS
+//#define GYRO_1000DPS
+//#define GYRO_2000DPS
+
+//Uncomment only one full scale accelerometer range (G's)
+#define ACCEL_2G //Default
+//#define ACCEL_4G
+//#define ACCEL_8G
+//#define ACCEL_16G
+
+#define SDA_PIN   21
+#define SCL_PIN   44
+
+#if defined GYRO_250DPS
+  #define GYRO_SCALE GYRO_FS_SEL_250
+  #define GYRO_SCALE_FACTOR 131.0
+#elif defined GYRO_500DPS
+  #define GYRO_SCALE GYRO_FS_SEL_500
+  #define GYRO_SCALE_FACTOR 65.5
+#elif defined GYRO_1000DPS
+  #define GYRO_SCALE GYRO_FS_SEL_1000
+  #define GYRO_SCALE_FACTOR 32.8
+#elif defined GYRO_2000DPS
+  #define GYRO_SCALE GYRO_FS_SEL_2000
+  #define GYRO_SCALE_FACTOR 16.4
+#endif
+
+#if defined ACCEL_2G
+  #define ACCEL_SCALE ACCEL_FS_SEL_2
+  #define ACCEL_SCALE_FACTOR 16384.0
+#elif defined ACCEL_4G
+  #define ACCEL_SCALE ACCEL_FS_SEL_4
+  #define ACCEL_SCALE_FACTOR 8192.0
+#elif defined ACCEL_8G
+  #define ACCEL_SCALE ACCEL_FS_SEL_8
+  #define ACCEL_SCALE_FACTOR 4096.0
+#elif defined ACCEL_16G
+  #define ACCEL_SCALE ACCEL_FS_SEL_16
+  #define ACCEL_SCALE_FACTOR 2048.0
+#endif
+
+//========================================================================================================================//
+//                                               USER-SPECIFIED VARIABLES                                                 //                           
+//========================================================================================================================//
+
+//Controller parameters (take note of defaults before modifying!): 
+float i_limit = 25.0;     //Integrator saturation level, mostly for safety (default 25.0)
+float maxRoll = 30.0;     //Max roll angle in degrees for angle mode (maximum ~70 degrees), deg/sec for rate mode 
+float maxPitch = 30.0;    //Max pitch angle in degrees for angle mode (maximum ~70 degrees), deg/sec for rate mode
+float maxYaw = 160.0;     //Max yaw rate in deg/sec
+
+//IMU calibration parameters - calibrate IMU using calculate_IMU_error() in the void setup() to get these values, then comment out calculate_IMU_error()
+float AccErrorX = -0.08;
+float AccErrorY = 0.00;
+float AccErrorZ = -0.06;
+float GyroErrorX = -2.76;
+float GyroErrorY = -1.93;
+float GyroErrorZ = 0.20;
+
+//========================================================================================================================//
+//                                              DECLARING GLOBAL VARIABLES                                                //                           
+//========================================================================================================================//
+//IMU:
+float AccX, AccY, AccZ;
+float AccX_prev, AccY_prev, AccZ_prev;
+float GyroX, GyroY, GyroZ;
+float GyroX_prev, GyroY_prev, GyroZ_prev;
+float MagX, MagY, MagZ;
+float MagX_prev, MagY_prev, MagZ_prev;
+float roll_IMU, pitch_IMU, yaw_IMU;
+float roll_IMU_prev, pitch_IMU_prev;
+float q0 = 1.0f; //Initialize quaternion for madgwick filter
+float q1 = 0.0f;
+float q2 = 0.0f;
+float q3 = 0.0f;
+
+//========================================================================================================================//
+//                                                      VOID SETUP                                                        //                           
+//========================================================================================================================//
+
+void setup(void) {
+  Serial.begin(115200);
+
+  Wire.begin(SDA_PIN, SCL_PIN);  // changing the SDA and SCL pins due to T-Display
+
+  //Initialize IMU communication
+  IMUinit();
+
+  while (!Serial)
+    delay(10); // pausing until the serial console is open
+
+  Serial.println("Path tracking test");
+
+  Serial.println("");
+
+  Serial.println("Calibrating...");
+
+  calculate_IMU_error();
+
+  Serial.println("");
+
+  Serial.println("Calibration done!");
+
+  // Serial.println("Time (s),Gyroscope X (deg/s),Gyroscope Y (deg/s),Gyroscope Z (deg/s),Accelerometer X (g),Accelerometer Y (g),Accelerometer Z (g)");
+  // delay(100);
+}
+
+//========================================================================================================================//
+//                                                       MAIN LOOP                                                        //                           
+//========================================================================================================================//
+
+void loop() {
+
+  
+}
+
+//========================================================================================================================//
+//                                                      FUNCTIONS                                                         //                           
+//========================================================================================================================//
+
+void IMUinit() {
+  //DESCRIPTION: Initialize IMU
+  /*
+   * Don't worry about how this works.
+   */
+#if defined USE_MPU6050_I2C
+    Wire.begin();
+    Wire.setClock(1000000); // 2.5x spec sheet 400 kHz max...
+    
+    mpu6050.initialize();
+    
+    if (mpu6050.testConnection() == false) {
+      Serial.println("MPU6050 initialization unsuccessful");
+      Serial.println("Check MPU6050 wiring or try cycling power");
+      while(1) {}
+    }
+
+    mpu6050.setFullScaleGyroRange(GYRO_SCALE);
+    mpu6050.setFullScaleAccelRange(ACCEL_SCALE);
+#endif
+}
+
+void calculate_IMU_error() {
+  //DESCRIPTION: Computes IMU accelerometer and gyro error on startup. Note: vehicle should be powered up on flat surface
+  /*
+   * Don't worry too much about what this is doing. The error values it computes are applied to the raw gyro and 
+   * accelerometer values AccX, AccY, AccZ, GyroX, GyroY, GyroZ in getIMUdata(). This eliminates drift in the
+   * measurement. 
+   */
+  int16_t AcX,AcY,AcZ,GyX,GyY,GyZ,MgX,MgY,MgZ;
+  AccErrorX = 0.0;
+  AccErrorY = 0.0;
+  AccErrorZ = 0.0;
+  GyroErrorX = 0.0;
+  GyroErrorY= 0.0;
+  GyroErrorZ = 0.0;
+  
+  //Read IMU values 12000 times
+  int c = 0;
+  while (c < 12000) {
+    #if defined USE_MPU6050_I2C
+      mpu6050.getMotion6(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ);
+    #elif defined USE_MPU9250_SPI
+      mpu9250.getMotion9(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ, &MgX, &MgY, &MgZ);
+    #endif
+    
+    AccX  = AcX / ACCEL_SCALE_FACTOR;
+    AccY  = AcY / ACCEL_SCALE_FACTOR;
+    AccZ  = AcZ / ACCEL_SCALE_FACTOR;
+    GyroX = GyX / GYRO_SCALE_FACTOR;
+    GyroY = GyY / GYRO_SCALE_FACTOR;
+    GyroZ = GyZ / GYRO_SCALE_FACTOR;
+    
+    //Sum all readings
+    AccErrorX  = AccErrorX + AccX;
+    AccErrorY  = AccErrorY + AccY;
+    AccErrorZ  = AccErrorZ + AccZ;
+    GyroErrorX = GyroErrorX + GyroX;
+    GyroErrorY = GyroErrorY + GyroY;
+    GyroErrorZ = GyroErrorZ + GyroZ;
+    c++;
+  }
+  //Divide the sum by 12000 to get the error value
+  AccErrorX  = AccErrorX / c;
+  AccErrorY  = AccErrorY / c;
+  AccErrorZ  = AccErrorZ / c - 1.0;
+  GyroErrorX = GyroErrorX / c;
+  GyroErrorY = GyroErrorY / c;
+  GyroErrorZ = GyroErrorZ / c;
+
+  Serial.print("float AccErrorX = ");
+  Serial.print(AccErrorX);
+  Serial.println(";");
+  Serial.print("float AccErrorY = ");
+  Serial.print(AccErrorY);
+  Serial.println(";");
+  Serial.print("float AccErrorZ = ");
+  Serial.print(AccErrorZ);
+  Serial.println(";");
+  
+  Serial.print("float GyroErrorX = ");
+  Serial.print(GyroErrorX);
+  Serial.println(";");
+  Serial.print("float GyroErrorY = ");
+  Serial.print(GyroErrorY);
+  Serial.println(";");
+  Serial.print("float GyroErrorZ = ");
+  Serial.print(GyroErrorZ);
+  Serial.println(";");
+
+  Serial.println("Paste these values in user specified variables section and comment out calculate_IMU_error() in void setup.");
+}
