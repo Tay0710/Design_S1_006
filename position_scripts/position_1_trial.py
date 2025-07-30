025dd8f2a774d8105cfd3d230d27d4b704c515cd
TayTay0710 <taylah0710@gmail.com>
2025-07-30 14:16:38 +0800

    Updated to include the removal of  gravity from acceleration using quaternion rotation.

--- position_scripts/position_1_trial.py
+++ position_scripts/position_1_trial.py
@@ -35,10 +35,33 @@ def calculate_orientation(full_data, time_step):
         )
                 
         quaternions.append(quaternion)
-        euler_angles.append(quaternion.to_euler())  # roll, pitch, yaw
+        # roll, pitch, yaw
+        euler_angles.append(quaternion.to_euler())
 
     # don't love the np part, find another way
     return np.array(euler_angles), quaternions
+
+def rotate_vector_by_quaternion(v, q):
+    # w is a scalar and x. y. z are vector components
+    w, qx, qy, qz = q.w, q.x, q.y, q.z
+    q_vec = np.array([qx, qy, qz])
+    # First cross product: first rotation
+    cross_1 = np.cross(q_vec, v)
+    # Second cross product: secondary rotation
+    cross_2 = np.cross(q_vec, cross_1)
+    # Final rotated vector
+    rotated_gravity = v + 2 * (w * cross_1 + cross_2)
+    return rotated_gravity
+
+def remove_gravity(acceleration_data, quaternions):
+    linear_accelerations = []
+    gravity_global = np.array([0, 0, 1])
+    
+    for a, q in zip(acceleration_data, quaternions):
+        rotated_gravity = rotate_vector_by_quaternion(gravity_global, q)
+        lin_accel = a - rotated_gravity
+        linear_accelerations.append(lin_accel)
+    return linear_accelerations
     
 def main():
     full_data = load_sensor_data("../sensor_logs/sensor_data.csv")
@@ -49,9 +72,16 @@ def main():
     print(f"Sample rate: {sample_rate:.2f} Hz")
     print(f"Time step: {time_step:.6f} s")
     
-    eulers, quats = calculate_orientation(full_data, time_step)
+    eulers, quaternions = calculate_orientation(full_data, time_step)
     print("\nFirst 5 Euler angles (roll, pitch, yaw in radians):")
     print(eulers[:5])
+    
+    acceleration = full_data[['Accelerometer X (g)', 'Accelerometer Y (g)', 'Accelerometer Z (g)']].values
+    lin_accels = remove_gravity(acceleration, quaternions)
+    
+    print("\nFirst 5 linear accelerations (gravity removed):")
+    for i in range(5):
+        print(f"{lin_accels[i]}")
     
 if __name__ == "__main__":
     main()
