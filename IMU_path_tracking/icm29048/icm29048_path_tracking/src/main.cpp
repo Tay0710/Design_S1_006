8282baf2552502782c70b3c8183be21800d0a505
Sam27315 <141702190+Sam27315@users.noreply.github.com>
2025-08-07 12:57:21 +0800

    Testing serial monitor

--- IMU_path_tracking/icm29048/icm29048_path_tracking/src/main.cpp
+++ IMU_path_tracking/icm29048/icm29048_path_tracking/src/main.cpp
@@ -1,10 +1,10 @@
-#include "ICM_20948.h" // SparkFun ICM-20948 Library
+#include <ICM_20948.h> // SparkFun ICM-20948 Library
 #include <Wire.h>
 
 #define SDA_PIN   21
 #define SCL_PIN   44
 
-float accelXOffset, accelYOffset, accelZOffset, rollOffset, pitchOffset, yawOffset; // Calibration floats
+// float accelXOffset, accelYOffset, accelZOffset, rollOffset, pitchOffset, yawOffset; // Calibration floats
 
 //#define USE_SPI       // Uncomment this to use SPI
 
@@ -13,9 +13,7 @@ float accelXOffset, accelYOffset, accelZOffset, rollOffset, pitchOffset, yawOffs
 #define SPI_PORT SPI // Your desired SPI port.       Used only when "USE_SPI" is defined
 #define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
 
-#define WIRE_PORT Wire // Your desired Wire port.      Used when "USE_SPI" is not defined
-// The value of the last bit of the I2C address.
-// On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
+#define WIRE_PORT Wire 
 #define AD0_VAL 1
 
 #ifdef USE_SPI
@@ -26,341 +24,351 @@ ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
 
 void setup()
 {
-
   SERIAL_PORT.begin(115200);
-  while (!SERIAL_PORT)
-  {
-  };
 
-#ifdef USE_SPI
-  SPI_PORT.begin();
-#else
-  WIRE_PORT.begin();
-  WIRE_PORT.setClock(400000);
-#endif
+  Wire.begin(SDA_PIN, SCL_PIN);  // changing the SDA and SCL pins due to T-Display
 
-  //myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+  while (!SERIAL_PORT){};
 
-  bool initialized = false;
-  while (!initialized)
-  {
+  #ifdef USE_SPI
+    SPI_PORT.begin();
+  #else
+    WIRE_PORT.begin();
+    WIRE_PORT.setClock(400000);
+    myICM.begin( WIRE_PORT, AD0_VAL ); // initialises ICM-20948
+  #endif
+  
+  SERIAL_PORT.println("");
+  delay(5000);
+  SERIAL_PORT.println("Serial initialized");
+  delay(5000);
+  SERIAL_PORT.println("Serial initialized");
+  delay(5000);
+  SERIAL_PORT.println("Serial initialized");
 
-#ifdef USE_SPI
-    myICM.begin(CS_PIN, SPI_PORT);
-#else
-    myICM.begin(WIRE_PORT, AD0_VAL);
-#endif
+//   //myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
 
-    SERIAL_PORT.print(F("Initialization of the sensor returned: "));
-    SERIAL_PORT.println(myICM.statusString());
-    if (myICM.status != ICM_20948_Stat_Ok)
-    {
-      SERIAL_PORT.println("Trying again...");
-      delay(500);
-    }
-    else
-    {
-      initialized = true;
-    }
-  }
+//   bool initialized = false;
+//   while (!initialized)
+//   {
+
+// #ifdef USE_SPI
+//     myICM.begin(CS_PIN, SPI_PORT);
+// #else
+//     myICM.begin(WIRE_PORT, AD0_VAL);
+// #endif
+
+//     SERIAL_PORT.print(F("Initialization of the sensor returned: "));
+//     SERIAL_PORT.println(myICM.statusString());
+//     if (myICM.status != ICM_20948_Stat_Ok)
+//     {
+//       SERIAL_PORT.println("Trying again...");
+//       delay(500);
+//     }
+//     else
+//     {
+//       initialized = true;
+//     }
+//   }
 }
 
 void loop()
 {
-
-  if (myICM.dataReady())
-  {
-    myICM.getAGMT();         // The values are only updated when you call 'getAGMT'
-                             //    printRawAGMT( myICM.agmt );     // Uncomment this to see the raw values, taken directly from the agmt structure
-    printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
-    delay(30);
-  }
-  else
-  {
-    SERIAL_PORT.println("Waiting for data");
-    delay(500);
-  }
-}
-
-// Below here are some helper functions to print the data nicely!
-
-void printPaddedInt16b(int16_t val)
-{
-  if (val > 0)
-  {
-    SERIAL_PORT.print(" ");
-    if (val < 10000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (val < 1000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (val < 100)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (val < 10)
-    {
-      SERIAL_PORT.print("0");
-    }
-  }
-  else
-  {
-    SERIAL_PORT.print("-");
-    if (abs(val) < 10000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (abs(val) < 1000)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (abs(val) < 100)
-    {
-      SERIAL_PORT.print("0");
-    }
-    if (abs(val) < 10)
-    {
-      SERIAL_PORT.print("0");
-    }
-  }
-  SERIAL_PORT.print(abs(val));
-}
-
-void printRawAGMT(ICM_20948_AGMT_t agmt)
-{
-  SERIAL_PORT.print("RAW. Acc [ ");
-  printPaddedInt16b(agmt.acc.axes.x);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.acc.axes.y);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.acc.axes.z);
-  SERIAL_PORT.print(" ], Gyr [ ");
-  printPaddedInt16b(agmt.gyr.axes.x);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.gyr.axes.y);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.gyr.axes.z);
-  SERIAL_PORT.print(" ], Mag [ ");
-  printPaddedInt16b(agmt.mag.axes.x);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.mag.axes.y);
-  SERIAL_PORT.print(", ");
-  printPaddedInt16b(agmt.mag.axes.z);
-  SERIAL_PORT.print(" ], Tmp [ ");
-  printPaddedInt16b(agmt.tmp.val);
-  SERIAL_PORT.print(" ]");
-  SERIAL_PORT.println();
-}
-
-void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
-{
-  float aval = abs(val);
-  if (val < 0)
-  {
-    SERIAL_PORT.print("-");
-  }
-  else
-  {
-    SERIAL_PORT.print(" ");
-  }
-  for (uint8_t indi = 0; indi < leading; indi++)
-  {
-    uint32_t tenpow = 0;
-    if (indi < (leading - 1))
-    {
-      tenpow = 1;
-    }
-    for (uint8_t c = 0; c < (leading - 1 - indi); c++)
-    {
-      tenpow *= 10;
-    }
-    if (aval < tenpow)
-    {
-      SERIAL_PORT.print("0");
-    }
-    else
-    {
-      break;
-    }
-  }
-  if (val < 0)
-  {
-    SERIAL_PORT.print(-val, decimals);
-  }
-  else
-  {
-    SERIAL_PORT.print(val, decimals);
-  }
-}
-
-#ifdef USE_SPI
-void printScaledAGMT(ICM_20948_SPI *sensor)
-{
-#else
-void printScaledAGMT(ICM_20948_I2C *sensor)
-{
-#endif
-  SERIAL_PORT.print("Scaled. Acc (mg) [ ");
-  printFormattedFloat(sensor->accX(), 5, 2);
-  SERIAL_PORT.print(", ");
-  printFormattedFloat(sensor->accY(), 5, 2);
-  SERIAL_PORT.print(", ");
-  printFormattedFloat(sensor->accZ(), 5, 2);
-  SERIAL_PORT.print(" ], Gyr (DPS) [ ");
-  printFormattedFloat(sensor->gyrX(), 5, 2);
-  SERIAL_PORT.print(", ");
-  printFormattedFloat(sensor->gyrY(), 5, 2);
-  SERIAL_PORT.print(", ");
-  printFormattedFloat(sensor->gyrZ(), 5, 2);
-  SERIAL_PORT.print(" ], Mag (uT) [ ");
-  printFormattedFloat(sensor->magX(), 5, 2);
-  SERIAL_PORT.print(", ");
-  printFormattedFloat(sensor->magY(), 5, 2);
-  SERIAL_PORT.print(", ");
-  printFormattedFloat(sensor->magZ(), 5, 2);
-  SERIAL_PORT.print(" ], Tmp (C) [ ");
-  printFormattedFloat(sensor->temp(), 5, 2);
-  SERIAL_PORT.print(" ]");
-  SERIAL_PORT.println();
-}
-
-
-Adafruit_MPU6050 mpu;
-
-void setup(void) {
-  Serial.begin(115200);
-
-  Wire.begin(SDA_PIN, SCL_PIN);  // changing the SDA and SCL pins due to T-Display
-
-  while (!Serial)
-    delay(10); // pausing until the serial console is open
-
-  Serial.println("Path tracking test");
-
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  Serial.println("MPU6050 Found!");
-
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  Serial.println("");
-
-  Serial.println("Calibrating...");
-  int num_avg;
-  
-  for (num_avg = 0; num_avg < 1000; num_avg++) {
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-    accelXOffset += a.acceleration.x;
-    accelYOffset += a.acceleration.y;
-    accelZOffset += a.acceleration.z;
-    rollOffset += g.gyro.x;
-    pitchOffset += g.gyro.y;
-    yawOffset += g.gyro.z;
-    delay(1);
-  }
-
-  accelXOffset = accelXOffset/num_avg;
-  accelYOffset = accelYOffset/num_avg;
-  accelZOffset = accelZOffset/num_avg - 1;
-  rollOffset = rollOffset/num_avg;
-  pitchOffset = pitchOffset/num_avg;
-  yawOffset = yawOffset/num_avg;
-
-  Serial.println("Calibration done!");
-
-  Serial.println("");
-
-  Serial.println("Time (s),Gyroscope X (deg/s),Gyroscope Y (deg/s),Gyroscope Z (deg/s),Accelerometer X (g),Accelerometer Y (g),Accelerometer Z (g)");
   delay(100);
-}
 
-void loop() {
+//   if (myICM.dataReady())
+//   {
+//     myICM.getAGMT();         // The values are only updated when you call 'getAGMT'
+//                              //    printRawAGMT( myICM.agmt );     // Uncomment this to see the raw values, taken directly from the agmt structure
+//     printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+//     delay(30);
+//   }
+//   else
+//   {
+//     SERIAL_PORT.println("Waiting for data");
+//     delay(500);
+//   }
+// }
 
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  float timeStamp = millis()/1000.0;
+// // Below here are some helper functions to print the data nicely!
+
+// void printPaddedInt16b(int16_t val)
+// {
+//   if (val > 0)
+//   {
+//     SERIAL_PORT.print(" ");
+//     if (val < 10000)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//     if (val < 1000)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//     if (val < 100)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//     if (val < 10)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//   }
+//   else
+//   {
+//     SERIAL_PORT.print("-");
+//     if (abs(val) < 10000)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//     if (abs(val) < 1000)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//     if (abs(val) < 100)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//     if (abs(val) < 10)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//   }
+//   SERIAL_PORT.print(abs(val));
+// }
+
+// void printRawAGMT(ICM_20948_AGMT_t agmt)
+// {
+//   SERIAL_PORT.print("RAW. Acc [ ");
+//   printPaddedInt16b(agmt.acc.axes.x);
+//   SERIAL_PORT.print(", ");
+//   printPaddedInt16b(agmt.acc.axes.y);
+//   SERIAL_PORT.print(", ");
+//   printPaddedInt16b(agmt.acc.axes.z);
+//   SERIAL_PORT.print(" ], Gyr [ ");
+//   printPaddedInt16b(agmt.gyr.axes.x);
+//   SERIAL_PORT.print(", ");
+//   printPaddedInt16b(agmt.gyr.axes.y);
+//   SERIAL_PORT.print(", ");
+//   printPaddedInt16b(agmt.gyr.axes.z);
+//   SERIAL_PORT.print(" ], Mag [ ");
+//   printPaddedInt16b(agmt.mag.axes.x);
+//   SERIAL_PORT.print(", ");
+//   printPaddedInt16b(agmt.mag.axes.y);
+//   SERIAL_PORT.print(", ");
+//   printPaddedInt16b(agmt.mag.axes.z);
+//   SERIAL_PORT.print(" ], Tmp [ ");
+//   printPaddedInt16b(agmt.tmp.val);
+//   SERIAL_PORT.print(" ]");
+//   SERIAL_PORT.println();
+// }
+
+// void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
+// {
+//   float aval = abs(val);
+//   if (val < 0)
+//   {
+//     SERIAL_PORT.print("-");
+//   }
+//   else
+//   {
+//     SERIAL_PORT.print(" ");
+//   }
+//   for (uint8_t indi = 0; indi < leading; indi++)
+//   {
+//     uint32_t tenpow = 0;
+//     if (indi < (leading - 1))
+//     {
+//       tenpow = 1;
+//     }
+//     for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+//     {
+//       tenpow *= 10;
+//     }
+//     if (aval < tenpow)
+//     {
+//       SERIAL_PORT.print("0");
+//     }
+//     else
+//     {
+//       break;
+//     }
+//   }
+//   if (val < 0)
+//   {
+//     SERIAL_PORT.print(-val, decimals);
+//   }
+//   else
+//   {
+//     SERIAL_PORT.print(val, decimals);
+//   }
+// }
+
+// #ifdef USE_SPI
+// void printScaledAGMT(ICM_20948_SPI *sensor)
+// {
+// #else
+// void printScaledAGMT(ICM_20948_I2C *sensor)
+// {
+// #endif
+//   SERIAL_PORT.print("Scaled. Acc (mg) [ ");
+//   printFormattedFloat(sensor->accX(), 5, 2);
+//   SERIAL_PORT.print(", ");
+//   printFormattedFloat(sensor->accY(), 5, 2);
+//   SERIAL_PORT.print(", ");
+//   printFormattedFloat(sensor->accZ(), 5, 2);
+//   SERIAL_PORT.print(" ], Gyr (DPS) [ ");
+//   printFormattedFloat(sensor->gyrX(), 5, 2);
+//   SERIAL_PORT.print(", ");
+//   printFormattedFloat(sensor->gyrY(), 5, 2);
+//   SERIAL_PORT.print(", ");
+//   printFormattedFloat(sensor->gyrZ(), 5, 2);
+//   SERIAL_PORT.print(" ], Mag (uT) [ ");
+//   printFormattedFloat(sensor->magX(), 5, 2);
+//   SERIAL_PORT.print(", ");
+//   printFormattedFloat(sensor->magY(), 5, 2);
+//   SERIAL_PORT.print(", ");
+//   printFormattedFloat(sensor->magZ(), 5, 2);
+//   SERIAL_PORT.print(" ], Tmp (C) [ ");
+//   printFormattedFloat(sensor->temp(), 5, 2);
+//   SERIAL_PORT.print(" ]");
+//   SERIAL_PORT.println();
+// }
+
+
+// Adafruit_MPU6050 mpu;
+
+// void setup(void) {
+//   Serial.begin(115200);
+
+//   Wire.begin(SDA_PIN, SCL_PIN);  // changing the SDA and SCL pins due to T-Display
+
+//   while (!Serial)
+//     delay(10); // pausing until the serial console is open
+
+//   Serial.println("Path tracking test");
+
+//   // Try to initialize!
+//   if (!mpu.begin()) {
+//     Serial.println("Failed to find MPU6050 chip");
+//     while (1) {
+//       delay(10);
+//     }
+//   }
+//   Serial.println("MPU6050 Found!");
+
+//   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+//   Serial.print("Accelerometer range set to: ");
+//   switch (mpu.getAccelerometerRange()) {
+//   case MPU6050_RANGE_2_G:
+//     Serial.println("+-2G");
+//     break;
+//   case MPU6050_RANGE_4_G:
+//     Serial.println("+-4G");
+//     break;
+//   case MPU6050_RANGE_8_G:
+//     Serial.println("+-8G");
+//     break;
+//   case MPU6050_RANGE_16_G:
+//     Serial.println("+-16G");
+//     break;
+//   }
+//   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+//   Serial.print("Gyro range set to: ");
+//   switch (mpu.getGyroRange()) {
+//   case MPU6050_RANGE_250_DEG:
+//     Serial.println("+- 250 deg/s");
+//     break;
+//   case MPU6050_RANGE_500_DEG:
+//     Serial.println("+- 500 deg/s");
+//     break;
+//   case MPU6050_RANGE_1000_DEG:
+//     Serial.println("+- 1000 deg/s");
+//     break;
+//   case MPU6050_RANGE_2000_DEG:
+//     Serial.println("+- 2000 deg/s");
+//     break;
+//   }
+
+//   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+//   Serial.print("Filter bandwidth set to: ");
+//   switch (mpu.getFilterBandwidth()) {
+//   case MPU6050_BAND_260_HZ:
+//     Serial.println("260 Hz");
+//     break;
+//   case MPU6050_BAND_184_HZ:
+//     Serial.println("184 Hz");
+//     break;
+//   case MPU6050_BAND_94_HZ:
+//     Serial.println("94 Hz");
+//     break;
+//   case MPU6050_BAND_44_HZ:
+//     Serial.println("44 Hz");
+//     break;
+//   case MPU6050_BAND_21_HZ:
+//     Serial.println("21 Hz");
+//     break;
+//   case MPU6050_BAND_10_HZ:
+//     Serial.println("10 Hz");
+//     break;
+//   case MPU6050_BAND_5_HZ:
+//     Serial.println("5 Hz");
+//     break;
+//   }
+
+//   Serial.println("");
+
+//   Serial.println("Calibrating...");
+//   int num_avg;
   
-  /* Print out the values */
-  Serial.print(timeStamp, 4);
-  Serial.print(",");
-  Serial.print(g.gyro.x - rollOffset, 4);
-  Serial.print(",");
-  Serial.print(g.gyro.y - pitchOffset, 4);
-  Serial.print(",");
-  Serial.print(g.gyro.z - yawOffset, 4);
-  Serial.print(",");
-  Serial.print(a.acceleration.x - accelXOffset, 4);
-  Serial.print(",");
-  Serial.print(a.acceleration.y - accelYOffset, 4);
-  Serial.print(",");
-  Serial.println(a.acceleration.z - accelZOffset, 4);
-  delay(5);
+//   for (num_avg = 0; num_avg < 1000; num_avg++) {
+//     sensors_event_t a, g, temp;
+//     mpu.getEvent(&a, &g, &temp);
+//     accelXOffset += a.acceleration.x;
+//     accelYOffset += a.acceleration.y;
+//     accelZOffset += a.acceleration.z;
+//     rollOffset += g.gyro.x;
+//     pitchOffset += g.gyro.y;
+//     yawOffset += g.gyro.z;
+//     delay(1);
+//   }
+
+//   accelXOffset = accelXOffset/num_avg;
+//   accelYOffset = accelYOffset/num_avg;
+//   accelZOffset = accelZOffset/num_avg - 1;
+//   rollOffset = rollOffset/num_avg;
+//   pitchOffset = pitchOffset/num_avg;
+//   yawOffset = yawOffset/num_avg;
+
+//   Serial.println("Calibration done!");
+
+//   Serial.println("");
+
+//   Serial.println("Time (s),Gyroscope X (deg/s),Gyroscope Y (deg/s),Gyroscope Z (deg/s),Accelerometer X (g),Accelerometer Y (g),Accelerometer Z (g)");
+//   delay(100);
+// }
+
+// void loop() {
+
+//   /* Get new sensor events with the readings */
+//   sensors_event_t a, g, temp;
+//   mpu.getEvent(&a, &g, &temp);
+//   float timeStamp = millis()/1000.0;
+  
+//   /* Print out the values */
+//   Serial.print(timeStamp, 4);
+//   Serial.print(",");
+//   Serial.print(g.gyro.x - rollOffset, 4);
+//   Serial.print(",");
+//   Serial.print(g.gyro.y - pitchOffset, 4);
+//   Serial.print(",");
+//   Serial.print(g.gyro.z - yawOffset, 4);
+//   Serial.print(",");
+//   Serial.print(a.acceleration.x - accelXOffset, 4);
+//   Serial.print(",");
+//   Serial.print(a.acceleration.y - accelYOffset, 4);
+//   Serial.print(",");
+//   Serial.println(a.acceleration.z - accelZOffset, 4);
+//   delay(5);
   
 }
