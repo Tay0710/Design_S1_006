414f58409abbff0efdc2c371f988ac30dd359c26
Dorothy Jong <dorothy.jong@hotmail.com>
2025-08-09 22:20:04 +0800

    added mac address for esp home

---
+++ MAC_ADDRESS/src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <esp_wifi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+void setup(){
+  Serial.begin(115200);
+
+  delay(1000);
+
+  Serial.print("ESP Board MAC Address: ");
+  #ifdef ESP32
+    WiFi.mode(WIFI_STA);
+    WiFi.begin();
+    uint8_t baseMac[6];
+    esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+    if (ret == ESP_OK) {
+      Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                    baseMac[0], baseMac[1], baseMac[2],
+                    baseMac[3], baseMac[4], baseMac[5]);
+    } else {
+      Serial.println("Failed to read MAC address");
+    }
+  #else
+    Serial.println(WiFi.macAddress());
+  #endif
+}
+ 
+void loop(){
+
+  // lilygo-t-display-s3 -> ESP Board MAC Address: 30:30:f9:59:c7:28
+  // ESP32 -> 44:1d:64:f8:12:0c
+
+}
