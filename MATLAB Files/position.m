9da5e1d1c8f3a5aab267dbee829a2b0805bbc049
TayTay0710 <taylah0710@gmail.com>
2025-08-16 10:23:33 +0800

    Updated to include conversion of gyro and accel. Updated to include fuse function and plot of Orientation Estimate.

--- MATLAB Files/position.m
+++ MATLAB Files/position.m
@@ -4,8 +4,12 @@ data = readtable(filename);
 
 % Extract columns
 timestamp = data{:,1};  
-gyroscopeReadings    = data(:,2:4);
-accelerometerReadings= data(:,5:7);
+gyroscopeReadings    = data{:,2:4};
+accelerometerReadings= data{:,5:7};
+
+% Unit conversions
+accelerometerReadings = accelerometerReadings * 9.80665;
+gyroscopeReadings     = deg2rad(gyroscopeReadings);
 
 % Calculate sample rate from timestamps
 dt = mean(diff(timestamp))   % average time step
@@ -19,3 +23,18 @@ disp(accelerometerReadings(1:5, :));
 
 disp('First 5 gyroscope readings:');
 disp(gyroscopeReadings(1:5, :));
+
+% Create imufilter object
+decim = 1;
+fuse = imufilter('SampleRate',Fs,'DecimationFactor',decim);
+
+% Output an estimate of the sensor body orientation over time (quaternions)
+q = fuse(accelerometerReadings,gyroscopeReadings);
+
+% Plot the orientation (use actual timestamps)
+time = timestamp - timestamp(1);
+plot(time,eulerd(q,'ZYX','frame'))
+title('Orientation Estimate')
+legend('Z-axis', 'Y-axis', 'X-axis')
+xlabel('Time (s)')
+ylabel('Rotation (degrees)')
