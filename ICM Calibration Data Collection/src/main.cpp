6e467b3292f71f6ee39d718a756ade8cf581698d
Owen Spicer <23134383@student.uwa.edu.au>
2025-08-27 12:28:31 +0800

    Testing

--- ICM Calibration Data Collection/src/main.cpp
+++ ICM Calibration Data Collection/src/main.cpp
@@ -1,17 +1,16 @@
-// Record ICM-45686 IMU data to SD card when trigger pin is LOW
-// Host ESP32 serves a web page to download the CSV file
-
 #include <WiFi.h>
 #include <WebServer.h>
 #include <SD.h>
 #include <SPI.h>
 #include "ICM45686.h"
 
-ICM456xx IMU(SPI, 5);  // SPI CS on GPIO5
-SPIClass hspi(HSPI);   // Use HSPI for SD card
+// Use VSPI for both IMU and SD card
+#define IMU_CS   5   // IMU CS pin
+#define SD_CS    15  // SD card CS pin
 
-#define TRIGGER_PIN 4   // Recording switch
-#define SD_CS 15        // SD card CS
+ICM456xx IMU(SPI, IMU_CS);
+
+#define TRIGGER_PIN 4
 #define AP_SSID "ESP32_Frames"
 #define AP_PASSWORD "12345678"
 
@@ -29,30 +28,26 @@ void setup() {
 
   // Start Wi-Fi AP
   WiFi.softAP(AP_SSID, AP_PASSWORD);
-  Serial.print("AP started. Connect to: ");
-  Serial.println(AP_SSID);
-  Serial.print("IP: ");
-  Serial.println(WiFi.softAPIP());
+  Serial.printf("AP started. Connect to: %s\nIP: %s\n", AP_SSID, WiFi.softAPIP().toString().c_str());
 
-  // Initialize SPI for IMU
-  SPI.begin(18, 19, 23, 5); // SCK=18, MISO=19, MOSI=23, CS=5
+  // Initialize VSPI
+  SPI.begin(18, 19, 23); // SCK=18, MISO=19, MOSI=23
 
   // Initialize IMU
   if (IMU.begin() != 0) {
     Serial.println("ICM456xx initialization failed");
-    while(1);
+    while (1);
   }
-  IMU.startAccel(1600, 16);   // 100 Hz, ±16 g
-  IMU.startGyro(1600, 2000);  // 100 Hz, ±2000 dps
+  IMU.startAccel(100, 16);   // 100 Hz, ±16 g
+  IMU.startGyro(100, 2000);  // 100 Hz, ±2000 dps
 
-  // Initialize SPI for SD card
-  hspi.begin(14, 12, 13, SD_CS); // SCK=14, MISO=12, MOSI=13, CS=15
-  if (!SD.begin(SD_CS, hspi)) {
+  // Initialize SD card on same SPI bus but different CS
+  if (!SD.begin(SD_CS, SPI)) {
     Serial.println("SD init failed!");
-    while(1);
+    while (1);
   }
 
-  // Create CSV header
+  // CSV header
   SD.remove(filename);
   File file = SD.open(filename, FILE_WRITE);
   if (file) {
@@ -81,29 +76,23 @@ void setup() {
 void loop() {
   server.handleClient();
 
+  recording = (digitalRead(TRIGGER_PIN) == LOW);
+  if (!recording) return;
+
   inv_imu_sensor_data_t imu_data;
   IMU.getDataFromRegisters(imu_data);
 
-  recording = (digitalRead(TRIGGER_PIN) == LOW);
-
-  if(recording) {
-    File file = SD.open(filename, FILE_APPEND);
-    if(file) {
-      file.print(micros());
-      file.print(",");
-      file.print(imu_data.accel_data[0]*16.0/32768.0);
-      file.print(",");
-      file.print(imu_data.accel_data[1]*16.0/32768.0);
-      file.print(",");
-      file.print(imu_data.accel_data[2]*16.0/32768.0);
-      file.print(",");
-      file.print(imu_data.gyro_data[0]*2000.0/32768.0);
-      file.print(",");
-      file.print(imu_data.gyro_data[1]*2000.0/32768.0);
-      file.print(",");
-      file.print(imu_data.gyro_data[2]*2000.0/32768.0);
-      file.println();
-      file.close();
-    }
+  File file = SD.open(filename, FILE_APPEND);
+  if(file) {
+    file.printf("%lu,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n",
+      micros(),
+      imu_data.accel_data[0]*16.0/32768.0,
+      imu_data.accel_data[1]*16.0/32768.0,
+      imu_data.accel_data[2]*16.0/32768.0,
+      imu_data.gyro_data[0]*2000.0/32768.0,
+      imu_data.gyro_data[1]*2000.0/32768.0,
+      imu_data.gyro_data[2]*2000.0/32768.0
+    );
+    file.close();
   }
 }
