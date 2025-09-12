b2bec428fda730704ef0cd550a10dec8b865681c
Dorothy Jong <dorothy.jong@hotmail.com>
2025-09-12 23:39:17 +0800

    Wrote final lora code to be on drone

---
+++ LORA_DRONE_RECEIVER/src/LoRa_SX1262.h
@@ -0,0 +1,26 @@
+#include <SX126x-Arduino.h>
+#include <SPI.h>
+
+// ESP32-S3 - SX126x pin configuration
+int PIN_LORA_RESET = 1;	 // LORA RESET
+int PIN_LORA_DIO_1 = 2; // LORA DIO_1
+int PIN_LORA_BUSY = 3;	 // LORA SPI BUSY
+int PIN_LORA_NSS = 10;	 // LORA SPI CS
+int PIN_LORA_SCLK = 12;	 // LORA SPI CLK
+int PIN_LORA_MISO = 11;	 // LORA SPI MISO
+int PIN_LORA_MOSI = 13;	 // LORA SPI MOSI
+
+// Define LoRa parameters
+#define RF_FREQUENCY 915000000	// Hz 
+#define TX_OUTPUT_POWER 22		// dBm (Note: maximum power of SX1262 is 22 dBm)
+#define LORA_BANDWIDTH 0		// [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12], SF7 is fastest, SF12 is slowest but greatest range and more robust
+#define LORA_CODINGRATE 1		// [1: 4/5, 2: 4/6,  3: 4/7,  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8	// Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0	// Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define RX_TIMEOUT_VALUE 3000 
+#define TX_TIMEOUT_VALUE 5000 
+
+
---
+++ LORA_DRONE_RECEIVER/src/main.cpp
@@ -0,0 +1,141 @@
+#include <Arduino.h>
+#include <Ticker.h>
+
+#include "LoRa_SX1262.h"
+
+static RadioEvents_t RadioEvents;
+hw_config hwConfig;
+time_t lastMessage;
+
+Ticker loraTimer;
+
+#define FAILSAFE_LORA_TIMEOUT 5000 // ms
+#define LORA_TIMER_UDPATE_RATE 1000 // check every 1000 ms
+
+/**@brief Function to be executed on Radio Rx Timeout event
+ */
+void OnRxTimeout(void)
+{
+  Serial.println("On Rx Timeout CB");
+  Serial.printf("OnRxTimeout: No message received in last %d ms \n", RX_TIMEOUT_VALUE);
+
+  // TODO: check if failsafe is required if LORA never connects in the first place
+
+}
+
+/**@brief Function to be executed on Radio Rx Done event
+ */
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
+{
+  Serial.println("OnRxDone:");
+
+  // Time from last message
+  time_t now = millis();
+  double time_between_message = (now - lastMessage) / 1000.0;
+
+  Serial.print("Message: ");
+  Serial.println((const char *)payload);
+  Serial.printf("RssiValue=%d dBm, SnrValue=%d\n", rssi, snr);
+  Serial.printf("Time from last message=%.2f seconds \n", time_between_message);
+
+  lastMessage = millis();
+}
+
+/**@brief Function to be executed on Radio Rx Error event
+ */
+void OnRxError(void)
+{
+  Serial.println("OnRxError CB");
+}
+
+void setupLoRaModule()
+{
+  // Define the HW configuration between MCU and SX126x
+  hwConfig.CHIP_TYPE = SX1262_CHIP;         // Example uses an eByte E22 module with an SX1262
+  hwConfig.PIN_LORA_RESET = PIN_LORA_RESET; // LORA RESET // Note: times out way more often...
+  hwConfig.PIN_LORA_NSS = PIN_LORA_NSS;     // LORA SPI CS
+  hwConfig.PIN_LORA_SCLK = PIN_LORA_SCLK;   // LORA SPI CLK
+  hwConfig.PIN_LORA_MISO = PIN_LORA_MISO;   // LORA SPI MISO
+  hwConfig.PIN_LORA_DIO_1 = PIN_LORA_DIO_1; // LORA DIO_1
+  hwConfig.PIN_LORA_BUSY = PIN_LORA_BUSY;   // LORA SPI BUSY
+  hwConfig.PIN_LORA_MOSI = PIN_LORA_MOSI;   // LORA SPI MOSI
+  hwConfig.USE_DIO2_ANT_SWITCH = false;     // Example uses an CircuitRocks Alora RFM1262 which uses DIO2 pins as antenna control
+  hwConfig.USE_DIO3_TCXO = false;           // Example uses an CircuitRocks Alora RFM1262 which uses DIO3 to control oscillator voltage
+  hwConfig.USE_DIO3_ANT_SWITCH = false;     // Only Insight ISP4520 module uses DIO3 as antenna control
+  hwConfig.USE_RXEN_ANT_PWR = false;        // RXEN is used as power for antenna switch
+
+  // Initialize the LoRa chip
+  Serial.println("Starting lora_hardware_init");
+  uint32_t init_result = 0;
+
+  init_result = lora_hardware_init(hwConfig);
+  Serial.printf("LoRa init %s\r\n", init_result == 0 ? "success" : "failed");
+
+  // Initialize the Radio callbacks
+  RadioEvents.RxDone = OnRxDone;
+  RadioEvents.RxTimeout = OnRxTimeout;
+  RadioEvents.RxError = OnRxError;
+
+  // Initialize the Radio
+  Radio.Init(&RadioEvents);
+
+  // Set radio to sleep for next setup
+  Radio.Sleep();
+
+  // Set Radio channel
+  Radio.SetChannel(RF_FREQUENCY);
+
+  // Set Radio TX configuration
+  Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    true, 0, 0, LORA_IQ_INVERSION_ON, TX_TIMEOUT_VALUE);
+
+  // Set Radio RX configuration
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+
+  // Start LoRa
+  Serial.println("Starting Radio.Rx");
+  Radio.Rx(RX_TIMEOUT_VALUE); // Note: timeout only works for initial packet
+  // Receiver is non-blocking
+}
+
+void loraTimerCallback()
+{
+  time_t currentTime = millis();
+
+  if (currentTime - lastMessage > FAILSAFE_LORA_TIMEOUT)
+  {
+    Serial.printf("Communications lost to failsafe transceiver for %d ms. Land drone. \n", currentTime - lastMessage);
+    // rcChannels[AUX3] = 1800;
+  }
+}
+
+void setup()
+{
+  // Initialize Serial for debug output
+  Serial.begin(115200);
+
+  delay(1000); // Wait for serial to begin
+
+  Serial.println("=====================================");
+  Serial.println("LoRa Drone Receiver");
+  Serial.println("=====================================");
+
+  setupLoRaModule();
+
+  loraTimer.attach_ms(LORA_TIMER_UDPATE_RATE, loraTimerCallback);
+
+  lastMessage = millis();
+}
+
+void loop()
+{
+  // Radio.Rx(0); // Note: this is non-blocking
+  // Serial.println("Wait for message...");
+
+  delay(5000);
+}
