8a5c47fb9a46fad82f3a840c80c7578484eaa8dd
Sam27315 <141702190+Sam27315@users.noreply.github.com>
2025-09-21 15:05:57 +0800

    Removed alternate code

--- Ultrasonic_Sensors/MB1030_int/src/main.cpp
+++ Ultrasonic_Sensors/MB1030_int/src/main.cpp
@@ -78,47 +78,4 @@ void loop() {
     US2_ready = false;
   }
   digitalWrite(LOOP_ACTIVITY, LOW);
-
 }
-
-// #include <Arduino.h>
-
-// #define PW_PIN 14
-// #define MAX_SAMPLES 50
-
-// volatile unsigned long pulseStart = 0;
-// volatile unsigned long pulseBuffer[MAX_SAMPLES];
-// volatile uint8_t pulseWriteIndex = 0;
-// volatile bool newPulse = false;
-
-// void IRAM_ATTR pwISR() {
-//   if (digitalRead(PW_PIN)) {       // Rising edge
-//     pulseStart = micros();
-//   } else {                          // Falling edge
-//     unsigned long pw = micros() - pulseStart;
-//     pulseBuffer[pulseWriteIndex] = pw;
-//     pulseWriteIndex = (pulseWriteIndex + 1) % MAX_SAMPLES;
-//     newPulse = true;
-//   }
-// }
-
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(PW_PIN, INPUT);
-//   attachInterrupt(digitalPinToInterrupt(PW_PIN), pwISR, CHANGE);
-// }
-
-// void loop() {
-//   if (newPulse) {
-//     noInterrupts();                 // safely read buffer
-//     uint8_t index = (pulseWriteIndex + MAX_SAMPLES - 1) % MAX_SAMPLES;
-//     unsigned long pw = pulseBuffer[index];
-//     newPulse = false;
-//     interrupts();
-
-//     float distanceCm = pw / 58.0;  // MB1030 datasheet formula
-//     Serial.print("Distance: ");
-//     Serial.print(distanceCm);
-//     Serial.println(" cm");
-//   }
-// }
