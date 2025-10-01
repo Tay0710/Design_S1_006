aa34e35a35f2f8895d91fdea1cff5c51e4ca8a7b
O-Spicer <owenpspicer2002@gmail.com>
2025-10-01 11:41:24 +0800

    code for SDcard + module test

--- SDmodule/SDModuleTest/src/main.cpp
+++ SDmodule/SDModuleTest/src/main.cpp
@@ -1,18 +1,45 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// VSPI default pins: SCK=18, MISO=19, MOSI=23
+#define SD_CS 5   // Chip Select pin (safe choice)
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Starting SD card test with VSPI...");
+
+  // Init SD on VSPI (no need for custom SPIClass, defaults are used)
+  if (!SD.begin(SD_CS)) {
+    Serial.println("SD init failed!");
+    while (true);
+  }
+  Serial.println("SD card initialized.");
+
+  // Create TXT file
+  File txtFile = SD.open("/test.txt", FILE_WRITE);
+  if (txtFile) {
+    txtFile.println("Hello from ESP32 VSPI test");
+    txtFile.close();
+    Serial.println("test.txt created.");
+  } else {
+    Serial.println("Failed to create test.txt");
+  }
+
+  // Create CSV file
+  File csvFile = SD.open("/data.csv", FILE_WRITE);
+  if (csvFile) {
+    csvFile.println("time,value");
+    csvFile.println("0,123");
+    csvFile.close();
+    Serial.println("data.csv created.");
+  } else {
+    Serial.println("Failed to create data.csv");
+  }
+
+  Serial.println("Creation complete.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // nothing else to do
 }
