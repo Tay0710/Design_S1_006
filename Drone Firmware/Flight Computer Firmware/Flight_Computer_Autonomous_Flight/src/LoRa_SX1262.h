186e2ab21de54d9b7dc0680e360b3c04b0506ed8
Dorothy Jong <88929687+dj8389@users.noreply.github.com>
2025-10-28 15:00:35 +0800

    Add files via upload

---
+++ Drone Firmware/Flight Computer Firmware/Flight_Computer_Autonomous_Flight/src/LoRa_SX1262.h
@@ -0,0 +1,29 @@
+// Adapted from PingPong Example from SX126x-Arduino Library
+// https://github.com/beegee-tokyo/SX126x-Arduino/blob/master/examples/PingPong/PingPong.ino
+
+#include <SX126x-Arduino.h>
+#include <SPI.h>
+
+// ESP32-S3 - SX126x pin configuration
+int PIN_LORA_RESET = 9;	 // LORA RESET
+int PIN_LORA_DIO_1 = 14; // LORA DIO_1
+int PIN_LORA_BUSY = 21;	 // LORA SPI BUSY
+int PIN_LORA_NSS = 8;	 // LORA SPI CS
+int PIN_LORA_SCLK = 13;	 // LORA SPI CLK
+int PIN_LORA_MISO = 12;	 // LORA SPI MISO
+int PIN_LORA_MOSI = 11;	 // LORA SPI MOSI
+
+// Define LoRa parameters
+#define RF_FREQUENCY 915000000	// Hz 
+#define TX_OUTPUT_POWER 22		// dBm (Note: maximum power of SX1262 is 22 dBm)
+#define LORA_BANDWIDTH 0		// [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12], SF7 is fastest, SF12 is slowest but greatest range and more robust
+#define LORA_CODINGRATE 3		// [1: 4/5, 2: 4/6,  3: 4/7,  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 16	// Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0	// Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define RX_TIMEOUT_VALUE 5000 
+#define TX_TIMEOUT_VALUE 5000 
+
+
